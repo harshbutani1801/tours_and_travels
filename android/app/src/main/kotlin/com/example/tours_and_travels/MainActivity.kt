50e00501f4ddb6c15be59b6e71df416d8d7ecd48
Harsh Butani <bce23d2d21@vgec1.gujgov.edu.in>
2022-01-22 13:10:59 +0530

    Basic Application

---
+++ android/app/src/main/kotlin/com/example/tours_and_travels/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tours_and_travels
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:tours_and_travels/Pages/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HB Tours & Travels',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
